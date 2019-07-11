63a84842031a865c92bf82bea25afcc3d6f8b371
陈红伟 <chenhongwei@chenhongweideMacBook-Pro.local>
2019-07-11 17:01:37 +0800

    test2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <h1>Jenkins</h1>
+    <h1>Jenkins test2ß</h1>
     <HelloWorld msg="Welcome to Your Vue.js App,jenkinstest"/>
   </div>
 </template>
